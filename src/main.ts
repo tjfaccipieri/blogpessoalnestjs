1ea6d64e8731a8ea04905f5a22b84698a3e51c0a
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2023-06-22 11:23:18 -0300

    chore: added database configuration on app.module.ts / main.ts updated

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // ajuste de timezone
+  process.env.TZ = '-03:00';
+
+  // validação das entidades envolvidas nos requests
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+
+  await app.listen(8080);
 }
 bootstrap();
