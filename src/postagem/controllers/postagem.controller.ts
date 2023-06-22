7b0a4b85d7f3c1306e38f2f5260c2f405c77abe7
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2023-06-22 12:10:27 -0300

    chore: created getById and GetByTitulo functions

--- src/postagem/controllers/postagem.controller.ts
+++ src/postagem/controllers/postagem.controller.ts
@@ -1,14 +1,33 @@
-import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { PostagemService } from '../services/postagem.service';
 import { Postagem } from '../entities/postagem.entity';
 
 @Controller('/postagens')
 export class PostagemController {
-  constructor(private readonly postagemService:PostagemService){}
+  constructor(private readonly postagemService: PostagemService) {}
 
   @Get()
   @HttpCode(HttpStatus.OK)
   findAll(): Promise<Postagem[]> {
-    return this.postagemService.findAll()
+    return this.postagemService.findAll();
+  }
+
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  findById(@Param('id', ParseIntPipe) id: number): Promise<Postagem> {
+    return this.postagemService.findById(id);
+  }
+
+  @Get('/titulo/:titulo')
+  @HttpCode(HttpStatus.OK)
+  findByTitulo(@Param('titulo') titulo: string): Promise<Postagem[]> {
+    return this.postagemService.findByTitulo(titulo)
   }
 }
--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
-import {InjectRepository} from '@nestjs/typeorm'
-import { Repository } from 'typeorm';
-import {Postagem} from '../entities/postagem.entity';
+import { Injectable, HttpStatus, HttpException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ILike, Repository } from 'typeorm';
+import { Postagem } from '../entities/postagem.entity';
 @Injectable()
 export class PostagemService {
   constructor(
@@ -10,6 +10,30 @@ export class PostagemService {
   ) {}
 
   async findAll(): Promise<Postagem[]> {
-    return await this.postagemRepository.find()
+    return await this.postagemRepository.find();
   }
+
+  async findById(id: number): Promise<Postagem> {
+    let postagem = await this.postagemRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!postagem)
+      throw new HttpException('Postagem não encontrada', HttpStatus.NOT_FOUND);
+
+    return postagem;
+  }
+
+  async findByTitulo(titulo: string): Promise<Postagem[]> {
+    return await this.postagemRepository.find({
+      where: {
+        titulo: ILike(`%${titulo}%`)
+      }
+    })
+  }
+
+
+
 }
